5c15ec1d8a54c6eea15a690f31dd814a9d8d6a67
yuka <yuka@localhost>
2023-04-10 17:51:43 +0900

    Task1 Fチーム1次　林田

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -48,7 +48,19 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-
+		if (password.length()>=8 && password.matches("^[A-Za-z0-9]+$")) {
+		}  
+		else {
+			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください");
+		   }
+		
+		if (password.equals(passwordForCheck)) {
+		}
+		else {
+			model.addAttribute("errorMessage", "パスワードが一致しません。");
+			return "createAccount";
+		}
+		
 		
 		// パラメータで受け取ったアカウント情報をDtoに格納する。
 		UserInfo userInfo = new UserInfo();
