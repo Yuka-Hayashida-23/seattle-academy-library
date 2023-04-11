1a27b4d1f57b42371e11be1d19d42ac7e7f3685e
yuka <yuka@localhost>
2023-04-11 18:30:05 +0900

    Task2 一次コミット

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,7 +43,7 @@ import jp.co.seattle.library.service.UsersService;
 			UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 	
 			// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-			if (StringUtils.isEmpty(selectedUserInfo)) {
+			if (ObjectUtils.isEmpty(selectedUserInfo)) {
 				return "redirect:/home";
 			} else {
 				model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
