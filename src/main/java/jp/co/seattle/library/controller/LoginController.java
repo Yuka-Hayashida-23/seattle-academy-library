1af1c5f528ab2c19afb636c604f5ef3d58fcf195
yuka <yuka@localhost>
2023-04-12 12:21:39 +0900

    Task2 修正

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -1,6 +1,6 @@
-	package jp.co.seattle.library.controller;
-	
-	import org.slf4j.Logger;
+package jp.co.seattle.library.controller;
+
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,42 +12,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.UsersService;
-	
+
+/**
+ * ログインコントローラー
+ */
+@Controller /** APIの入り口 */
+public class LoginController {
+	final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@Autowired
+	private UsersService usersService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String first(Model model) {
+		return "login"; // jspファイル名
+	}
+
 	/**
-	 * ログインコントローラー
+	 * ログイン処理
+	 *
+	 * @param email    メールアドレス
+	 * @param password パスワード
+	 * @param model
+	 * @return ホーム画面に遷移
 	 */
-	@Controller /** APIの入り口 */
-	public class LoginController {
-		final static Logger logger = LoggerFactory.getLogger(LoginController.class);
-	
-		@Autowired
-		private UsersService usersService;
-	
-		@RequestMapping(value = "/", method = RequestMethod.GET)
-		public String first(Model model) {
-			return "login"; // jspファイル名
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+
+		// メアドとパスワードに一致するユーザー取得
+		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
+
+		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
+		if (!(ObjectUtils.isEmpty(selectedUserInfo))) {
+			return "redirect:/home";
+		} else {
+			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
+			return "login";
 		}
-	
-		/**
-		 * ログイン処理
-		 *
-		 * @param email    メールアドレス
-		 * @param password パスワード
-		 * @param model
-		 * @return ホーム画面に遷移
-		 */
-		@RequestMapping(value = "/login", method = RequestMethod.POST)
-		public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
-	
-			// メアドとパスワードに一致するユーザー取得
-			UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
-	
-			// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-			if (ObjectUtils.isEmpty(selectedUserInfo)) {
-				return "redirect:/home";
-			} else {
-				model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
-				return "login";
-			}
 	}
-	}
+}
